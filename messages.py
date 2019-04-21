927db40c1fc7105fdc1ebfb75ed100a7fc41f835
gobisa <gobisa@umich.edu>
2019-04-21 19:49:54 -0400

    reduced cron message frequency

--- messages.py
+++ messages.py
@@ -2,14 +2,14 @@ from messaging import send_message
 import random
 
 def LA_time():
-    if random.randrange(2) == 0:
+    if random.randrange(7) == 0:
 	    send_message("9:08, LA time")
 
 def meat_show():
 	send_message("welcome to the meat show")
 
 def five_o_clock():
-    if random.randrange(10) == 0:
+    if random.randrange(30) == 0:
 	    send_message("It's 5 o'clock in the morning")
 
 def test_scheduler():
