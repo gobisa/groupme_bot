dc7cc405daa06da3bedcd722b103102e79333e2c
gobisa <gobisa@umich.edu>
2019-04-23 23:26:52 -0400

    send entire contents of message even if longer than max length

--- messaging.py
+++ messaging.py
@@ -12,14 +12,15 @@ except KeyError:
 #BOT_ID = TEST_BOT_ID
 
 
-def send_message(text):
-	if len(text) > 1000:
-		text = text[:1000]
-	print("sending: {}".format(text))
-	requests.post("https://api.groupme.com/v3/bots/post", data={
-		"bot_id"  : BOT_ID,
-		"text"    : text
-	})
+def send_message(text_in):
+    print("sending: {}".format(text_in))
+    MAX_LENGTH = 1000
+    text_array = [text_in[i:i+MAX_LENGTH] for i in range(0, len(text_in), MAX_LENGTH)]
+    for text in text_array:
+        requests.post("https://api.groupme.com/v3/bots/post", data={
+            "bot_id"  : BOT_ID,
+            "text"    : text
+        })
 
 # liking is not supported by bots in groupme
 '''
