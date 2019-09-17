e2f89ec236c04f2d09567bdc046e42f6be73a8ee
gobisa <gobisa@umich.edu>
2019-09-16 21:20:13 -0400

    shut off

--- server.py
+++ server.py
@@ -58,6 +58,7 @@ def index():
 
 @APP.route('/incoming_message', methods=['GET', 'POST'])
 def incoming_message():
+    return
     """Handle incoming messages."""
     if flask.request.method == 'GET':
         return 'page loaded'
